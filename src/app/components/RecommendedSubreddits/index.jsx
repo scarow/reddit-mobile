9acf9723724bdd5c5927747b639deaf1b6350164
scarow <samantha.carow@gmail.com>
2016-10-04 13:38:55 -0700

    add utm tracking

--- src/app/components/RecommendedSubreddits/index.jsx
+++ src/app/components/RecommendedSubreddits/index.jsx
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
 
 import map from 'lodash/map';
+import url from 'url';
 
 export const RecommendedSubreddits = (props) => {
   const { recommendedSubreddits, cssClass, variant, currentSubreddit } = props;
@@ -21,10 +22,10 @@ export const RecommendedSubreddits = (props) => {
   );
 
   if (variant == 'topPlain'){
-    subredditListing = map(recommendedSubreddits, sr => {
+    subredditListing = map(recommendedSubreddits, (sr, index) => {
       return (
         <div className={ cssClass }>
-          <a href={sr.url} className='sr-url'> { formatSubredditHref(sr.url) } </a>
+          <a href={addUtmTracking(sr.url, index, variant)} className='sr-url'> { formatSubredditHref(sr.url) } </a>
         </div>
       )
     })
@@ -33,15 +34,15 @@ export const RecommendedSubreddits = (props) => {
     subredditHeader = true;
     subredditListing = (
       <div className={ cssClass }>
-        <a href={currentSubreddit.url} className='sr-url'> { 'See more at ' + formatSubredditHref(currentSubreddit.url) } </a>
+        <a href={addUtmTracking(currentSubreddit.url, 0, variant)} className='sr-url'> { 'See more at ' + formatSubredditHref(currentSubreddit.url) } </a>
       </div>
     );
   } else {
-    subredditListing = map(recommendedSubreddits, sr => {
+    subredditListing = map(recommendedSubreddits, (sr, index) => {
       return (
         <div className={ cssClass }>
           <div className='subreddit-icon-image' style={ sr.iconImage ? { 'backgroundImage': `url(${sr.iconImage})`, 'backgroundPosition': '-1px 0px' } : {} }/>
-          <a href={sr.url} className='sr-url'> { formatSubredditHref(sr.url) } </a>
+          <a href={addUtmTracking(sr.url, index, variant)} className='sr-url'> { formatSubredditHref(sr.url) } </a>
           <div className='sr-subscriber-count'> { Number(sr.subscribers).toLocaleString('en') } { sr.subscribers > 1 ? 'subscribers' : 'subscriber' } </div>
         </div>
       )
@@ -54,6 +55,24 @@ export const RecommendedSubreddits = (props) => {
       { subredditListing }
     </div>
   );
+};
+
+
+function addUtmTracking(urlString, position, variant) {
+  const urlObject = url.parse(urlString, true);
+
+  return url.format({
+    pathname: urlObject.pathname,
+    query: { // we're appending utm params to the query. to do so we need to
+      // splat the query object from the parsed url and add the utm params
+      ...urlObject.query,
+      utm_source: 'mweb',
+      utm_medium: 'sr_recommendations',
+      utm_name: 'experiment_70',
+      utm_content: position,
+      utm_term: variant,
+    },
+  });
 };
 
 function formatSubredditHref(url){
